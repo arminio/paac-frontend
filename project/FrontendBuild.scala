a31a6799b55807e40b3af13a0ba1b8bff19bf717
Rambabu Posa <rambabu.posa@digital.hmrc.gov.uk>
2016-02-24 10:40:38 +0000

    PAAC-17:Create PAAC Microservices Templates

---
+++ project/FrontendBuild.scala
@@ -0,0 +1,54 @@
+import sbt._
+
+object FrontendBuild extends Build with MicroService {
+
+  val appName = "paac-frontend"
+
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+}
+
+private object AppDependencies {
+  import play.PlayImport._
+  import play.core.PlayVersion
+
+  private val playHealthVersion = "1.1.0"    
+  private val playJsonLoggerVersion = "2.1.1"      
+  private val frontendBootstrapVersion = "6.1.0"
+  private val govukTemplateVersion = "4.0.0"
+  private val playUiVersion = "4.9.0"
+  private val playPartialsVersion = "4.2.0"
+  private val playAuthorisedFrontendVersion = "4.5.0"
+  private val playConfigVersion = "2.0.1"
+  
+  val compile = Seq(
+    ws,
+    "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion,
+    "uk.gov.hmrc" %% "play-partials" % playPartialsVersion,
+    "uk.gov.hmrc" %% "play-authorised-frontend" % playAuthorisedFrontendVersion,
+    "uk.gov.hmrc" %% "play-config" % playConfigVersion,
+    "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
+    "uk.gov.hmrc" %% "govuk-template" % govukTemplateVersion,
+    "uk.gov.hmrc" %% "play-health" % playHealthVersion,
+    "uk.gov.hmrc" %% "play-ui" % playUiVersion
+  )
+
+  trait TestDependencies {
+    lazy val scope: String = "test"
+    lazy val test : Seq[ModuleID] = ???
+  }
+
+  object Test {
+    def apply() = new TestDependencies {
+      override lazy val test = Seq(
+        "org.scalatest" %% "scalatest" % "2.2.2" % scope,
+        "org.pegdown" % "pegdown" % "1.4.2" % scope,
+        "org.jsoup" % "jsoup" % "1.7.3" % scope,
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+      )
+    }.test
+  }
+
+  def apply() = compile ++ Test()
+}
+
+
---
+++ project/MicroService.scala
@@ -0,0 +1,69 @@
+import sbt.Keys._
+import sbt.Tests.{SubProcess, Group}
+import sbt._
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin._
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+trait MicroService {
+
+  import uk.gov.hmrc.DefaultBuildSettings._
+  import TestPhases._
+  import uk.gov.hmrc.SbtAutoBuildPlugin
+
+  val appName: String
+
+  lazy val appDependencies : Seq[ModuleID] = ???
+  lazy val plugins : Seq[Plugins] = Seq(play.PlayScala)
+  lazy val playSettings : Seq[Setting[_]] = Seq.empty
+
+  lazy val microservice = Project(appName, file("."))
+    .enablePlugins(plugins : _*)
+    .settings(playSettings : _*)
+    .settings(scalaSettings: _*)
+    .settings(publishingSettings: _*)
+    .settings(defaultSettings(): _*)
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
+    .enablePlugins(SbtDistributablesPlugin)
+    .settings(
+      targetJvm := "jvm-1.8",
+      libraryDependencies ++= appDependencies,
+      parallelExecution in Test := false,
+      fork in Test := false,
+      retrieveManaged := true
+    )
+    .settings(Repositories.playPublishingSettings : _*)
+    .configs(IntegrationTest)
+    .settings(inConfig(IntegrationTest)(Defaults.itSettings): _*)
+    .settings(
+      Keys.fork in IntegrationTest := false,
+      unmanagedSourceDirectories in IntegrationTest <<= (baseDirectory in IntegrationTest)(base => Seq(base / "it")),
+      addTestReportOption(IntegrationTest, "int-test-reports"),
+      testGrouping in IntegrationTest := oneForkedJvmPerTest((definedTests in IntegrationTest).value),
+      parallelExecution in IntegrationTest := false)
+    .disablePlugins(sbt.plugins.JUnitXmlReportPlugin)
+    .settings(resolvers += Resolver.bintrayRepo("hmrc", "releases"))
+}
+
+private object TestPhases {
+
+  def oneForkedJvmPerTest(tests: Seq[TestDefinition]) =
+    tests map {
+      test => new Group(test.name, Seq(test), SubProcess(ForkOptions(runJVMOptions = Seq("-Dtest.name=" + test.name))))
+    }
+}
+
+private object Repositories {
+
+  import uk.gov.hmrc._
+  import PublishingSettings._
+
+  lazy val playPublishingSettings : Seq[sbt.Setting[_]] = sbtrelease.ReleasePlugin.releaseSettings ++ Seq(
+
+    credentials += SbtCredentials,
+
+    publishArtifact in(Compile, packageDoc) := false,
+    publishArtifact in(Compile, packageSrc) := false
+  ) ++
+    publishAllArtefacts
+}
