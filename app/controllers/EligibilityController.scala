b2a6ccec2139110cd2fdb73c87f4d5e357fc7b43
Periklis <periklas@hotmail.com>
2016-04-01 14:52:37 +0100

    PAAC-169: Improved onSubmit code in EligibilityController

--- app/controllers/EligibilityController.scala
+++ app/controllers/EligibilityController.scala
@@ -28,23 +28,26 @@ object EligibilityController extends EligibilityController{
 }
 
 trait EligibilityController  extends BaseFrontendController {
-  val connector: CalculatorConnector
+  val connector: EligibilityController
   val keystore: KeystoreService
 
-  val onPageLoad:Action[AnyContent] = withSession { implicit request =>
+  private val onSubmitRedirect: Call = routes.SelectSchemeController.onPageLoad()
+
+  val onPageLoad = withSession { implicit request =>
     Future.successful(Ok(views.html.eligibility(EligibilityForm.form)))
   }
 
-  val onSubmit:Action[AnyContent] = withSession { implicit request =>
+  val onSubmit = withSession { implicit request =>
     EligibilityForm.form.bindFromRequest().fold(
       formWithErrors => { Future.successful(Ok(views.html.eligibility(EligibilityForm.form))) },
       input => {
         keystore.store[String](input, EligibilityForm.Eligibility)
-        Future.successful(Ok(views.html.selectScheme(SelectSchemeForm.form)))
+        Future.successful(Redirect(onSubmitRedirect))
       }
     )
   }
-  val onBack:Action[AnyContent] = Action.async { implicit request =>
+
+  val onBack = Action.async { implicit request =>
     Future.successful(Ok(views.html.startPage("")))
   }
 }
