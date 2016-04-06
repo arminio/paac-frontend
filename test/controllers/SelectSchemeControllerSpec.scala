57ce1d6202e47c63167451be7533530f42a1c8db
Rambabu Posa <rambabu.posa@digital.hmrc.gov.uk>
2016-04-06 14:29:28 +0100

    PAAC-170: Added unit tests for SelectSchemeController

---
+++ test/controllers/SelectSchemeControllerSpec.scala
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import java.util.UUID
+import org.scalatest.BeforeAndAfterAll
+import play.api.Play
+import play.api.mvc.{ Request, Result}
+import play.api.test.Helpers._
+import play.api.test.{FakeApplication, FakeRequest}
+import service.KeystoreService
+import uk.gov.hmrc.play.http.{HeaderCarrier, SessionKeys}
+import uk.gov.hmrc.play.test.UnitSpec
+import scala.concurrent.Future
+
+class SelectSchemeControllerSpec extends UnitSpec with BeforeAndAfterAll {
+  val app = FakeApplication()
+  val SESSION_ID = s"session-${UUID.randomUUID}"
+  val endPointURL = "/paac/scheme"
+
+  override def beforeAll() {
+    Play.start(app)
+    super.beforeAll() // To be stackable, must call super.beforeEach
+  }
+
+  override def afterAll() {
+    try {
+      super.afterAll()
+    } finally Play.stop()
+  }
+
+  implicit val request = FakeRequest()
+
+  trait MockSelectSchemeControllerFixture extends MockKeystoreFixture{
+    object MockSelectSchemeController extends SelectSchemeController {
+      override val keystore: KeystoreService = MockKeystore
+    }
+  }
+
+  trait MockKeystoreFixture {
+    object MockKeystore extends KeystoreService {
+      var map = Map(SessionKeys.sessionId -> SESSION_ID)
+      override def store[T](data: T, key: String)
+                           (implicit hc: HeaderCarrier,
+                            format: play.api.libs.json.Format[T],
+                            request: Request[Any])
+      : Future[Option[T]] = {
+        map = map + (key -> data.toString)
+        Future.successful(Some(data))
+      }
+      override def read[T](key: String)
+                          (implicit hc: HeaderCarrier,
+                           format: play.api.libs.json.Format[T],
+                           request: Request[Any])
+      : Future[Option[T]] = {
+        Future.successful((map get key).map(_.asInstanceOf[T]))
+      }
+    }
+  }
+
+  "SelectSchemeController" when {
+    "GET" should {
+      "not return result NOT_FOUND" in {
+        val result: Option[Future[Result]] = route(FakeRequest(GET, endPointURL))
+        result.isDefined shouldBe true
+        status(result.get) should not be NOT_FOUND
+      }
+
+      "return 303 for valid GET request" in {
+        val result: Option[Future[Result]] = route(FakeRequest(GET, endPointURL))
+        status(result.get) shouldBe 303
+      }
+
+      "not return 200 for valid GET request" in {
+        val result: Option[Future[Result]] = route(FakeRequest(GET, endPointURL))
+        status(result.get) should not be 200
+      }
+    }
+
+    "POST" should {
+      "not return result NOT_FOUND" in {
+        val result: Option[Future[Result]] = route(FakeRequest(POST, endPointURL))
+        result.isDefined shouldBe true
+        status(result.get) should not be NOT_FOUND
+      }
+
+      "return 303 for valid GET request" in {
+        val result: Option[Future[Result]] = route(FakeRequest(POST, endPointURL))
+        status(result.get) shouldBe 303
+      }
+
+      "with valid schemeType should save to keystore" in new MockSelectSchemeControllerFixture with MockKeystoreFixture{
+        // set up
+        implicit val hc = HeaderCarrier()
+        implicit val request = FakeRequest(POST, endPointURL).withSession((SessionKeys.sessionId,SESSION_ID)).withFormUrlEncodedBody(("schemeType" -> "db"))
+
+        // test
+        val result: Future[Result] = MockSelectSchemeController.onSubmit()(request)
+
+        // check
+        status(result) shouldBe 303
+        MockKeystore.map should contain key ("schemeType")
+        MockKeystore.map should contain value ("db")
+      }
+    }
+  }
+}
