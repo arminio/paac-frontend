442237f90ec44e5732285b99e808cd2b7d95d892
Periklis <periklas@hotmail.com>
2016-04-22 15:01:56 +0100

    PAAC-297: Added Controller, html and Spec files for TaxYearSelection Page. Edited messages and routes.

---
+++ test/controllers/TaxYearSelectionSpec.scala
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import java.util.UUID
+
+import org.scalatest.BeforeAndAfterAll
+import play.api.Play
+import play.api.mvc.Request
+import play.api.test.{FakeRequest, FakeApplication}
+import service.KeystoreService
+import uk.gov.hmrc.play.http.{HeaderCarrier, SessionKeys}
+import uk.gov.hmrc.play.test.UnitSpec
+import scala.concurrent.Future
+
+class TaxYearSelectionSpec extends UnitSpec with BeforeAndAfterAll {
+  val app = FakeApplication()
+  val SESSION_ID = s"session-${UUID.randomUUID}"
+  // TODO: chagnge the endpoint url
+  val endPointURL = "/paac/"
+
+  override def beforeAll() {
+    Play.start(app)
+    super.beforeAll() // To be stackable, must call super.beforeEach
+  }
+
+  override def afterAll() {
+    try {
+      super.afterAll()
+    } finally Play.stop()
+  }
+
+  implicit val request = FakeRequest()
+
+//  trait ControllerWithMockKeystore extends MockKeystoreFixture{
+//    object MockSelectSchemeControllerWithMockKeystore extends SelectSchemeController {
+//      override val keystore: KeystoreService = MockKeystore
+//    }
+//  }
+
+  trait MockKeystoreFixture {
+    object MockKeystore extends KeystoreService {
+      var map = Map(SessionKeys.sessionId -> SESSION_ID)
+      override def store[T](data: T, key: String)
+                           (implicit hc: HeaderCarrier,
+                            format: play.api.libs.json.Format[T],
+                            request: Request[Any])
+      : Future[Option[T]] = {
+        map = map + (key -> data.toString)
+        Future.successful(Some(data))
+      }
+      override def read[T](key: String)
+                          (implicit hc: HeaderCarrier,
+                           format: play.api.libs.json.Format[T],
+                           request: Request[Any])
+      : Future[Option[T]] = {
+        Future.successful((map get key).map(_.asInstanceOf[T]))
+      }
+    }
+  }
+
+  "TaxYearSelection" when {
+  }
+
+}
