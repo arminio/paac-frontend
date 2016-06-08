29d02be08ec84d7c331976a08355281fcb481b0f
Leanne Northrop <leanne.northrop@gmail.com>
2016-06-08 11:14:52 +0100

    PAAC-159: Fixed flow of Edit -> Back -> Back.

--- app/controllers/ReviewTotalAmountsController.scala
+++ app/controllers/ReviewTotalAmountsController.scala
@@ -20,6 +20,7 @@ import connector.CalculatorConnector
 import form._
 import play.api.mvc._
 import service._
+import service.KeystoreService._
 import uk.gov.hmrc.play.http.HeaderCarrier
 import models._
 import scala.concurrent.Future
@@ -38,17 +39,17 @@ trait ReviewTotalAmountsController extends RedirectController with models.ThisYe
   def fetchAmounts()(implicit hc: HeaderCarrier, request: Request[AnyContent]): Future[Map[String,String]] = {
     def yearAmountKeys(year: Int) : List[String] = year match {
       case y if y < 2015 =>
-        List(KeystoreService.DB_PREFIX + y, KeystoreService.DC_PREFIX + y)
+        List(DB_PREFIX + y, DC_PREFIX + y)
       case y if y == 2015 =>
-        List(KeystoreService.P1_DB_KEY,
-             KeystoreService.P1_DC_KEY,
-             KeystoreService.P2_DB_KEY,
-             KeystoreService.P2_DC_KEY,
-             KeystoreService.P1_TRIGGER_DC_KEY,
-             KeystoreService.P2_TRIGGER_DC_KEY)
+        List(P1_DB_KEY,
+             P1_DC_KEY,
+             P2_DB_KEY,
+             P2_DC_KEY,
+             P1_TRIGGER_DC_KEY,
+             P2_TRIGGER_DC_KEY)
       case y if y > 2015 =>
-        List(KeystoreService.DB_PREFIX + y, KeystoreService.DC_PREFIX + y, KeystoreService.TH_PREFIX + y,
-          KeystoreService.AI_PREFIX + y, KeystoreService.TA_PREFIX + y)
+        List(DB_PREFIX + y, DC_PREFIX + y, TH_PREFIX + y,
+          AI_PREFIX + y, TA_PREFIX + y)
       }
 
     keystore.read[String](List.range(2008, settings.THIS_YEAR + 1).flatMap(yearAmountKeys(_)))
@@ -56,38 +57,41 @@ trait ReviewTotalAmountsController extends RedirectController with models.ThisYe
 
   val onPageLoad = withSession { implicit request =>
     fetchAmounts.flatMap { (amountsMap) =>
-      keystore.read[String](KeystoreService.TRIGGER_DATE_KEY).map {
+      keystore.read[String](TRIGGER_DATE_KEY).flatMap {
         (td) =>
-        keystore.store(false.toString(), KeystoreService.IS_EDIT_KEY)
-        val values = amountsMap ++ Map((KeystoreService.TRIGGER_DATE_KEY, td.getOrElse("")))
-        val f = CalculatorForm.bind(values, true)
-        val model = f.get
-        val c = model.toPageValues.reverse.find(_.isTriggered)
-        CalculatorForm.bind(values).fold(
-          formWithErrors => {
-            Ok(views.html.review_amounts(formWithErrors, model.hasDefinedBenefits(), model.hasDefinedContributions(), model.hasTriggerDate(), c))
-          },
-          form => {
-            Ok(views.html.review_amounts(f, model.hasDefinedBenefits(), model.hasDefinedContributions(), model.hasTriggerDate(), c))
-          }
-        )
+        val result = keystore.save[String](List((Some(false.toString()), IS_EDIT_KEY), (Some("-1"), CURRENT_INPUT_YEAR_KEY)), "").map {
+          (_) =>
+          val values = amountsMap ++ Map((TRIGGER_DATE_KEY, td.getOrElse("")))
+          val f = CalculatorForm.bind(values, true)
+          val model = f.get
+          val c = model.toPageValues.reverse.find(_.isTriggered)
+          CalculatorForm.bind(values).fold(
+            formWithErrors => {
+              Ok(views.html.review_amounts(formWithErrors, model.hasDefinedBenefits(), model.hasDefinedContributions(), model.hasTriggerDate(), c))
+            },
+            form => {
+              Ok(views.html.review_amounts(f, model.hasDefinedBenefits(), model.hasDefinedContributions(), model.hasTriggerDate(), c))
+            }
+          )
+        }
+        result
       }
     }
   }
 
   def onEditAmount(year:Int) = withSession { implicit request =>
-    keystore.store(true.toString(), KeystoreService.IS_EDIT_KEY)
-    keystore.read(List(KeystoreService.TE_YES_NO_KEY)).flatMap {
+    keystore.store(true.toString(), IS_EDIT_KEY)
+    keystore.read(List(TE_YES_NO_KEY)).flatMap {
       (fieldsMap) =>
-      goTo(year, false, true, fieldsMap(KeystoreService.TE_YES_NO_KEY) == "Yes", Redirect(routes.ReviewTotalAmountsController.onPageLoad))
+      goTo(year, false, true, fieldsMap(TE_YES_NO_KEY) == "Yes", Redirect(routes.ReviewTotalAmountsController.onPageLoad))
     }
   }
 
   val onSubmit = withSession { implicit request =>
     fetchAmounts().flatMap { (amounts) =>
-      keystore.read[String](KeystoreService.TRIGGER_DATE_KEY).flatMap {
+      keystore.read[String](TRIGGER_DATE_KEY).flatMap {
         (td) =>
-        val values = amounts ++ Map((KeystoreService.TRIGGER_DATE_KEY, td.getOrElse("")))
+        val values = amounts ++ Map((TRIGGER_DATE_KEY, td.getOrElse("")))
         CalculatorForm.bind(values).fold(
           formWithErrors => {
             val f = CalculatorForm.bind(amounts, true)
@@ -100,9 +104,9 @@ trait ReviewTotalAmountsController extends RedirectController with models.ThisYe
             val contributions = input.toContributions()
             connector.connectToPAACService(contributions).flatMap{
               response =>
-              keystore.read[String](List(KeystoreService.DB_FLAG, KeystoreService.DC_FLAG)).flatMap {
+              keystore.read[String](List(DB_FLAG, DC_FLAG)).flatMap {
                 (fieldMap) =>
-                Future.successful(Ok(views.html.results(response, fieldMap(KeystoreService.DB_FLAG).toBoolean, fieldMap(KeystoreService.DC_FLAG).toBoolean)))
+                Future.successful(Ok(views.html.results(response, fieldMap(DB_FLAG).toBoolean, fieldMap(DC_FLAG).toBoolean)))
               }
             }
           }
